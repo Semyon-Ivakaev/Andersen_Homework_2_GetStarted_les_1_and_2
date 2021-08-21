110c253d645a7c968cc0d6088338af24dd1866b0
Semyon-Ivakaev <stixonforpython@gmail.com>
2021-08-21 18:02:34 +0300

    add feature/1.2

--- app/src/main/java/com/vertigo/helloworldapp/MainActivity.kt
+++ app/src/main/java/com/vertigo/helloworldapp/MainActivity.kt
@@ -2,17 +2,33 @@ package com.vertigo.helloworldapp
 
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var mCount = 0
+    private var countTextView: TextView? = null
+
+    companion object {
+        private val TAG_LOG = MainActivity::class.java.simpleName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         Log.d(TAG_LOG, "Hello World")
+
+        countTextView = findViewById(R.id.count_textView)
     }
 
+    fun showToast(view: View) {
+        Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show()
+    }
 
-    companion object {
-        private val TAG_LOG = MainActivity::class.java.simpleName
+    fun countUp(view: View) {
+        mCount++
+        countTextView?.text = mCount.toString()
     }
 }
