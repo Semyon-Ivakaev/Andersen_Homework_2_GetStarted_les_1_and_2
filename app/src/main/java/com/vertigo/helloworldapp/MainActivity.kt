c1343f692826a5dc3cfb2846e887e358c57fc57b
Semyon-Ivakaev <stixonforpython@gmail.com>
2021-08-21 14:18:43 +0300

    add fix LOG_TAG

--- app/src/main/java/com/vertigo/helloworldapp/MainActivity.kt
+++ app/src/main/java/com/vertigo/helloworldapp/MainActivity.kt
@@ -5,9 +5,12 @@ import android.os.Bundle
 import android.util.Log
 
 class MainActivity : AppCompatActivity() {
+
+    private val LOG_TAG = MainActivity()::class.java.simpleName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d("MainActivity", "Hello World")
+        Log.d(LOG_TAG, "Hello World")
     }
 }
