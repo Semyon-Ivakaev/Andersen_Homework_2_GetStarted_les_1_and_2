8737bd87b78cb2723a899270893822a6337802a0
Semyon-Ivakaev <stixonforpython@gmail.com>
2021-08-22 12:53:54 +0300

    add homework for feature/1.3 in master branch

---
+++ homework_1_3_textandscrollingviews/src/main/java/com/vertigo/homework_1_3_textandscrollingviews/MainActivityHomework.kt
@@ -0,0 +1,11 @@
+package com.vertigo.homework_1_3_textandscrollingviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivityHomework : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_homework)
+    }
+}
